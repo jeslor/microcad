0edd29464ad7949297aeabb1fad7828177d2e61d
Jeslor <jbestius@gmail.com>
2023-10-10 20:07:03 +0300

    final commit tuesday 10th October 2023

--- components/single/Hero/hero.tsx
+++ components/single/Hero/hero.tsx
@@ -4,7 +4,7 @@ import HeroSlider from './slider'
 
  function  LandingPage() {
   return(
-    <section className={`${styles.heroSlider} h-screen bg-dark-1`}>
+    <section className={`${styles.heroSlider} bg-dark-1`}>
       <div className={styles.heroBackground}>
         <Image src={'/static/media/backgrounds/gradient-bg.svg'} width={100} height={100} alt="herobg"  />
       </div>
@@ -13,7 +13,7 @@ import HeroSlider from './slider'
       </div>
      <div className={`${styles.sliderContainer} customwidth`}>
         <div className={`${styles.sliderContent}`}>
-          <h1 className="text-5xl font-bold text-white pb-3">We are a leading provider of IT solutions</h1>
+          <h1 className="text-5xl font-bold text-white pb-3">Microcad, a leading IT Solution in London</h1>
           <p className="text-white text-md">We are a leading provider of IT solutions Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, aut neque voluptas eligendi saepe nisi similique placeat cum nesciunt vero molestias ut excepturi nostrum, perspiciatis optio nihil illum veniam totam.</p>
           <button className="microcadBtn mt-4  ">Go shopping</button>
         </div>

f22e6bb46dd802404242f3c42e312885eb48c544
Jeslor <jbestius@gmail.com>
2023-10-11 17:46:21 +0300

    complete the slider functionality

--- components/single/Hero/slider.jsx
+++ components/single/Hero/slider.jsx
@@ -1,25 +1,57 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay,loop, Pagination, Scrollbar, Zoom } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import { sliderContnet } from '@/constants/slider';
 import styles from './slider.module.css'
+import './slider.css'
+import { Autoplay, Pagination, EffectCoverflow } from 'swiper/modules';
 
+import { sliderContnet } from '@/constants/slider';
 
 
 
 export default function HeroSlider() {
+
+
   return (
     <Swiper
-    className={styles.hersSlider}
-    spaceBetween={40}
-    slidesPerView={3}
-    pagination={{ clickable: true }}
-    autoplay={{ delay: 4000, disableOnInteraction: false, pauseOnMouseEnter: true, }}
+    className={`${styles.heroSlider} heroSlider`}
+    spaceBetween={30}
+    loop={true}
+    autoplay={{delay:2000}}
+    slidesPerView={3}           // added
+    speed={3000}  
+    pagination={{
+      clickable: true
+    }}
+    effect={'coverflow'}
+    grabCursor={true}
+    coverflowEffect={{
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    }}
+
+    modules={[Autoplay, Pagination, EffectCoverflow]}
+    breakpoints={{
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+
+    }}
     onSlideChange={() => console.log('slide change')}
-    onSwiper={(swiper) => console.log(swiper)}
+
   >
     {sliderContnet.map((item, index) => (
       <SwiperSlide key={index}>

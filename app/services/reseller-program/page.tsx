c6d21573e666815c946ecae42fdd56ce3dc3a3d5
Jeslor <jbestius@gmail.com>
2023-10-19 23:45:32 +0300

    complete reseller

--- app/services/reseller-program/page.tsx
+++ app/services/reseller-program/page.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+"use client";
+import styles from "@/styles/services.module.css"
+import { Icon } from "@iconify/react/dist/iconify.js"
 
 const Page = () => {
   return (
-    <div>
-        <h1>reseller-program</h1>
-    </div>
+    <div className={styles.contentHolder}>
+    <section className={`${styles.intro} text-slate-400 bg-primayColor`}>
+        <div className={styles.leftContent}>
+            <h1>Reseller Program</h1>
+            <p>IMPORTERS - EXPORTERS - RESELLERS Refurbished off lease equipment - large bulk purchases - Monitors, Laptops, Desktops We take care of logistics ship to your port of choice.
+            </p>
+            <p>For quotes on volume purchases and todays list of equipment email pamme@microcad.ca</p>
+        </div>
+        <div className={styles.rightContent}>
+            <div className={styles.imageHolder}>
+                <img src="/static/media/images/reseller.webp" alt="services" />
+            </div>
+        </div>
+    </section>
+
+   </div>
   )
 }
 

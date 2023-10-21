1b37c3c41cc786d6f917ceaaf1d3c699865345f8
Jeslor <jbestius@gmail.com>
2023-10-21 20:50:53 +0300

    complete the tables

--- app/policies/leasing-options/page.tsx
+++ app/policies/leasing-options/page.tsx
@@ -26,11 +26,19 @@ const page = () => {
             </p>
        </div>
       </section>
-      <section>
-
-      <div className={styles.table}>
+      <section className={`${styles.serviceDetail} text-slate-50`}>
+       <div className={`mt-0 pb-7 ${styles.category}`}>
+          <h3 className="!pb-1 text-primayColor">General commercial lease rates</h3>
+        </div>
+        <div className={styles.table}>
         <table>
           <thead>
+            <tr>
+              <th>RATE FACTOR PER $1000.</th>
+              <th>FAIR MARKET VALUE END OF TERM</th>
+            </tr>
+          </thead>
+          <thead className={styles.secondHead}>
             <tr>
               <th>Amount</th>
               <th>24 months</th>
@@ -91,9 +99,146 @@ const page = () => {
             </tr>
           </tbody>
         </table>
-      </div>
-        
-        
+        </div>
+        <div className={styles.table}>
+        <table>
+          <thead>
+            <tr>
+              <th>RATE FACTOR PER $1000.</th>
+              <th>$10.00 END OF TERM</th>
+            </tr>
+          </thead>
+          <thead className={styles.secondHead}>
+            <tr>
+              <th>Amount</th>
+              <th>24 months</th>
+              <th>36 months</th>
+              <th>48 months</th>
+              <th>60 months</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>$2,500 - $4,999</td>
+              <td>$48.01</td>
+              <td>$33.87</td>
+              <td>$26.85</td>
+              <td>NA</td>
+            </tr>
+            <tr>
+              <td>$5,000 - $9,999</td>
+              <td>$47.22</td>
+              <td>$33.01</td>
+              <td>$25.95</td>
+              <td>$21.74</td>
+            </tr>
+            <tr>
+              <td>$10,000 - $14,999</td>
+              <td>$46.43</td>
+              <td>$32.34</td>
+              <td>$25.32</td>
+              <td>$21.14</td>
+            </tr>
+            <tr>
+              <td>$15,000 - $19,999</td>
+              <td>$46.26</td>
+              <td>$32.15</td>
+              <td>$25.12</td>
+              <td>$20.93</td>
+            </tr>
+            <tr>
+              <td>$20,000 - $24,999</td>
+              <td>$45.88</td>
+              <td>$31.83</td>
+              <td>$24.83</td>
+              <td>$20.65</td>
+            </tr>
+            <tr>
+              <td>$25,000 - $49,999</td>
+              <td>$45.88</td>
+              <td>$31.83</td>
+              <td>$24.83</td>
+              <td>$20.65</td>
+            </tr>
+            <tr>
+              <td>END OF TERM</td>
+              <td>10.00</td>
+              <td>10.00</td>
+              <td>10.00</td>
+              <td>10.00</td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+        <div className={`${styles.table} ${styles.tablelast}`}>
+        <table>
+          <thead>
+            <tr>
+              <th>RATE FACTOR PER $1000.</th>
+              <th>STRETCH LEASE PAYMENT OPTION 10%</th>
+              <th>EARLY FMV AT END OF TERM</th>
+            </tr>
+          </thead>
+          <thead className={styles.secondHead}>
+            <tr>
+              <th>Amount</th>
+              <th>36/39 months</th>
+              <th>48/53 months</th>
+              <th>60/66 months</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>$2,500 - $4,999</td>
+              <td>$31.13</td>
+              <td>$24.76</td>
+              <td>NA</td>
+            </tr>
+            <tr>
+              <td>$5,000 - $9,999</td>
+              <td>$30.19</td>
+              <td>$23.79</td>
+              <td>$19.97</td>
+            </tr>
+            <tr>
+              <td>$10,000 - $14,999</td>
+              <td>$29.49</td>
+              <td>$23.14</td>
+              <td>$19.34</td>
+            </tr>
+            <tr>
+              <td>$15,000 - $19,999</td>
+              <td>$29.28</td>
+              <td>$22.92</td>
+              <td>$19.12</td>
+            </tr>
+            <tr>
+              <td>$20,000 - $24,999</td>
+              <td>$28.95</td>
+              <td>$22.61</td>
+              <td>$18.83</td>
+            </tr>
+            <tr>
+              <td>$25,000 - $49,999</td>
+              <td>$28.95</td>
+              <td>$22.61</td>
+              <td>$18.83</td>
+            </tr>
+            <tr>
+              <td>10% EARLY PAYMENT OPTION AT</td>
+              <td>36</td>
+              <td>48</td>
+              <td>60</td>
+            </tr>
+            <tr>
+              <td>END OF TERM OPTION</td>
+              <td>FMV</td>
+              <td>FMV</td>
+              <td>FMV</td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
       </section>
     </div>
   )

08b0496a71cdf5eebf158389d33759876be5574e
Jeslor <jbestius@gmail.com>
2023-10-22 19:49:05 +0300

    complete the first stage of the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,38 +3,77 @@ import styles from "@/styles/partners.module.css"
 const page = () => {
   return (
     <div className={styles.pageHeight}>
-        <div className={`${styles.header} bg-primayColor text-slate-200`}>
+        <div className={`${styles.contactHeader} bg-primayColor text-slate-200`}>
             <div className={styles.layer1}>
-                <img src="/static/media/backgrounds/partners.webp" alt="" />
+                {/* <img src="/static/media/backgrounds/partners.webp" alt="" /> */}
             </div>
-            <div className="layer2"></div>
-            <div className={` ${styles.title} customwidth`}>
-                <h1>Partners</h1>
-                <p>Microcad has developed many deep partnerships and strategic alliances within the Information Technology industry. Whether it's servers, desktops, laptops, network components or any number of other peripheral devices, Microcad can acquire, configure and deploy hardware from practically any manufacturer to meet your requirements. We have made it a "best practice" within our organization to help customers achieve the best possible solution by lending our expertise when it comes to negotiating with their chosen vendor partners.</p>
-                <div className={styles.imageHolder}>
-                    <img src="/static/media/images/partners.webp" alt="partners" />
+            <div className={`${styles.contact} customwidth`}>
+                <div className={` ${styles.contactParts}`}>
+                    <h1>Contact us</h1>
+                    <div className={styles.imageHolder}>
+                        <img src="/static/media/images/contactus.webp" alt="partners" />
+                    </div>
+                </div>
+                <div className={` ${styles.contactParts}`}>
+                    <h2>Location</h2>
+                    <ul className={`${styles.firstlevel} text-slate-400`}>
+                        <li><span>Address:</span>
+                         <div>
+                        <p>Microcad Computer Corporation</p>
+                        <p>4015 Dowell Drive</p>
+                        <p>London, ON</p>
+                        <p>N6L 1R1</p>
+                        <p>Canada</p>
+                            </div>
+                            </li>
+                        <li><span>Phone:</span> (519) 661-0355</li>
+                        <li><span>Fax:</span> (519) 661-0362</li>
+                    </ul>
+                </div>
+                <div className={` ${styles.contactParts}`}>
+                    <h2>Hours of operation</h2>
+                    <ul className={`${styles.firstlevel} text-slate-400`}>
+                        <li><span>Monday:</span> 9am - 5pm</li>
+                        <li><span>Tuesday:</span> 9am - 5pm</li>
+                        <li><span>Wednesday:</span> 9am - 5pm</li>
+                        <li><span>Thursday:</span> 9am - 5pm</li>
+                        <li><span>Friday:</span> 9am - 5pm</li>
+                        <li><span>Saturday & Sunday:</span> Closed</li>
+                    </ul>
+                </div>
+            </div> 
+            <div className={`${styles.contact} ${styles.secondContact} customwidth`}>
+                <span></span>
+                 <div className={` ${styles.contactParts}`}>
+                    <h2>Location</h2>
+                    <ul className={`${styles.firstlevel} text-slate-400`}>
+                        <li><span>Address:</span>
+                         <div>
+                        <p>Microcad Computer Corp</p>
+                        <p>45657 Port Street</p>
+                        <p>Plymouth, MI</p>
+                        <p>48170</p>
+                        <p>USA</p>
+                            </div>
+                            </li>
+                        <li><span>Phone(Toll free):</span> 1 (800) 948-3572</li>
+                  
+                    </ul>
+                </div>
+                <div className={` ${styles.contactParts}`}>
+                    <h2>Hours of operation</h2>
+                    <ul className={`${styles.firstlevel} text-slate-400`}>
+                        <li><span>Monday:</span> 9am - 5pm</li>
+                        <li><span>Tuesday:</span> 9am - 5pm</li>
+                        <li><span>Wednesday:</span> 9am - 5pm</li>
+                        <li><span>Thursday:</span> 9am - 5pm</li>
+                        <li><span>Friday:</span> 9am - 5pm</li>
+                        <li><span>Saturday & Sunday:</span> Closed</li>
+                    </ul>
                 </div>
             </div>
         </div>
-        <section className="customwidth mx-auto">
-            <div className={styles.partnerbarner}>
-                <h5>Microcad is an "Authorized Service Partner" for:</h5>
-                <div className={styles.partnerbarnerCompanies}>
-                    <div className={`${styles.logoHolder} bg-slate-300`}>
-                        <img src="/static/media/icons/lenovo.png" alt="" />
-                    </div>
-                    <div className={`${styles.logoHolder} bg-slate-300`}>
-                        <img src="/static/media/icons/toshiba.png" alt="" />
-                    </div>
-                    <div className={`${styles.logoHolder} bg-slate-300`}>
-                        <img src="/static/media/icons/acer.png" alt="" />
-                    </div>
-                    <div className={`${styles.logoHolder} bg-slate-300`}>
-                        <img src="/static/media/icons/xerox.png" alt="" />
-                    </div>
-                </div>
-            </div>
-        </section>
+   
     </div>
    
   )

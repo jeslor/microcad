43addcc7c7dea8a0d764a16b79b07d7d6c5daa9a
Jeslor <jbestius@gmail.com>
2023-10-22 14:14:05 +0300

    complete the customer service page

--- app/company/customer-service/page.tsx
+++ app/company/customer-service/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+import styles from "@/styles/services.module.css"
 
-const page = () => {
+const Page = () => {
   return (
-    <div>customer services</div>
+    <div className={styles.contentHolder}>
+    <section className={`${styles.intro} text-slate-400 bg-primayColor`}>
+        <div className={styles.leftContent}>
+            <h1>Customer service</h1>
+            <p>
+            Microcad has provided 31 years of exceptional customer service. Our no hassle policy has applied to all our clients no matter how large or small your purchase is. We pride ourselves in putting our customers first and ensuring your shopping experience is one that will have you coming back for more. Along with our price match policy you can rest assured Microcad is watching out for you. Our staff are trained, experienced and qualified to advise you on the latest trends in technology so your not alone when deciding on your purchase. Live help and free unlimited technical support is available at all times. Microcad the #1 technology partner.
+            </p>
+        </div>
+        <div className={styles.rightContent}>
+            <div className={styles.imageHolder}>
+                <img src="/static/media/images/customerservice.webp" alt="services" />
+            </div>
+        </div>
+
+    </section>
+
+   </div>
   )
 }
 
-export default page
+export default Page

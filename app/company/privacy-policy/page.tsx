1887642ae3896ad4f650db1a33125a60a50dbd1d
Jeslor <jbestius@gmail.com>
2023-10-22 16:07:10 +0300

    complete the testimonials

--- app/company/privacy-policy/page.tsx
+++ app/company/privacy-policy/page.tsx
@@ -1,8 +1,62 @@
-import React from 'react'
+"use client";
+import styles from "@/styles/services.module.css"
+import { Icon } from "@iconify/react/dist/iconify.js"
 
 const page = () => {
   return (
-    <div>privavy policy</div>
+   <div className={styles.contentHolder}>
+    <section className={`${styles.intro} text-slate-400 bg-primayColor`}>
+        <div className={styles.leftContent}>
+            <h1>Privacy policy</h1>
+            <p>
+            Microcad Computer Corporation is committed to protecting your privacy and developing technology that gives you the most powerful and safe online experience. This Statement of Privacy applies to the website microcad.ca and governs data collection and usage. By using the website, you consent to the data practices described in this statement.
+            </p>
+        </div>
+        <div className={styles.rightContent}>
+            <div className={styles.imageHolder}>
+                <img src="/static/media/images/privacypolicy.webp" alt="services" />
+            </div>
+        </div>
+    </section>
+    <section className={styles.listDivs}>
+    <h2 className="text-primayColor ">Collection of your Personal Information</h2>
+
+        <ul className="text-primarymedium">
+            <li>
+                <Icon icon="dashicons:privacy" />
+            <span>
+            Microcad Computer Corporation collects personally identifiable information, such as your e-mail address, name, home or work address or telephone number.
+            </span>
+            </li>
+            <li>
+                <Icon icon="dashicons:privacy" />
+                <span>
+                Microcad Computer Corporation collects personally identifiable information, such as your e-mail address, name, home or work address or telephone number.
+                </span>
+            </li>
+            <li>
+                <Icon icon="dashicons:privacy" />
+                <span>
+                Microcad Computer Corporation collects and uses your personal information to operate the Web site and deliver the services you have requested. Microcad Computer Corporation also uses your personally identifiable information to inform you of other products or services.
+                </span>
+            </li>
+            <li>
+                <Icon icon="dashicons:privacy" />
+                <span>
+                Microcad Computer Corporation Web sites will disclose your personal information, without notice, only if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on Microcad Computer Corporation or the site; (b) protect and defend the rights or property of Microcad Computer Corporation; and, (c) act under exigent circumstances to protect the personal safety of users of Microcad Computer Corporation, or the public.
+                Use of Cookies
+                </span>
+            </li>
+            <li>
+                <Icon icon="dashicons:privacy" />
+                <span>
+                The Microcad Computer Corporation Web site use “cookies” to help you personalize your online experience. A cookie is a text file that is placed on your hard disk by a Web page server. Cookies cannot be used to run programs or deliver viruses to your computer. Cookies are uniquely assigned to you, and can only be read by a web server in the domain that issued the cookie to you.
+                </span>
+            </li>
+        </ul>
+    </section>
+
+   </div>
   )
 }
 
--- app/company/testimonials/page.tsx
+++ app/company/testimonials/page.tsx
@@ -1,8 +1,143 @@
-import React from 'react'
+"use client";
+import styles from "@/styles/services.module.css"
+import { Icon } from "@iconify/react/dist/iconify.js"
 
 const page = () => {
   return (
-    <div>testimonials</div>
+   <div className={styles.contentHolder}>
+    <section className={`${styles.intro} text-slate-400 bg-primayColor`}>
+        <div className={styles.leftContent}>
+            <h1>Testimonials</h1>
+        </div>
+    </section>
+    <section className={styles.textimonials}>
+      <ul className="text-primaryLight">
+        <li>
+          <div className={styles.hide}></div>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/eidclinic.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+              "Microcad Computer Corporation is committed to protecting your privacy and developing technology that gives you the most powerful and safe online experience. This Statement of Privacy applies to the website microcad.ca and governs data collection and usage. By using the website, you consent to the data practices described in this statement."
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/londonssource.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            "I have been working with Microcad for the better part of the decade. Their dedication to service always exceeds my expectations, and their pricing is always better than the competition. I always send friends and family their way for their computer needs. Second to none!”
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/careercenter.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            "Over the past 15 years Microcad has been our one stop shop for all our technology needs. Always great pricing, fast service and knowledgeable friendly staff.""
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/londonlambeth.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            “Microcad has consistently exceeded expectations for our medical clinic from design to set up and install they have been professional and timely. They provide a professional service and good value consistently. They understand the importance of good customer relationships and continue to be an asset to our daily operations at the London Lambeth Clinic.”            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/techdz.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            “The professionalism and attention to detail on every project I work with Tim G & Brendan D. is the reason I keep coming back to Microcad all these years.”
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/mealsonwheel.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            "Microcad has provided our organization with excellent customer service and found the technology to meet our needs. This community minded business has helped us to ensure that we are able to meet the needs of those we serve. We highly recommend Microcad, their staff, services and tools." 
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/setupforkids.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            “Microcad has been amazing at answering all our questions and helping us set up with the non-profit program they have. They have helped Step Up help hundreds of families across western Canada who have been struggling through these crazy times. The products are always top notch and they stand by their work. Thank you Microcad for helping our vision come true!”
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/londonlambeth.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            “Microcad has consistently exceeded expectations for our medical clinic from design to set up and install they have been professional and timely. They provide a professional service and good value consistently. They understand the importance of good customer relationships and continue to be an asset to our daily operations at the London Lambeth Clinic.”            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/fanshawe.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            “We have been working with Microcad for almost a year now, during the height of the COVID pandemic.  They deliver excellent customer service and it’s been an enormous help to be able to provide our clients with reliable computers at a great price.”
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/goodwill.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            “Microcad has been very helpful in assisting Goodwill Amity by securing a steady stream of good quality, reasonably priced, refurbished laptops to deploy to our clients.  Especially during COVID times, being able to connect virtually has been a life line for many to continue with education, employment or finding a job and we are pleased that through this pandemic, we were able to assist individuals with these laptops so that they may continue to learn and reaching their career/job goals.”
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/atn.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            “Microcad, thank you for making our “not-so-ordinary” feel like business as usual!
+              Being a small non-profit, it’s been great working with a local big business that treats us just as big! At Microcad, we have never felt like a number, and we have certainly never felt like ‘the little guy.’ Quite the contrary! Having a direct line to Tim, who constantly goes above and beyond to customize services to meet the diverse needs of our amazing clients, has been a difference-maker. Beyond all that, Microcad, we also appreciate your willingness to not only host work experience placements through our employment training programs, but to walk-the-talk with your proactive, client-first approach to workplace training and accommodations.
+              Thank you Microcad! And thank you Tim! We are grateful for the work you do and the way you do it! Local businesses like you truly helps us to Do Opportunity Differently!”
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className={styles.imageHolder}>
+            <img src="/static/media/images/testimonials/huttonhouse.webp" alt="testimonial" />
+          </div>
+          <div className={styles.textHolder}>
+            <p className="text-sm">
+            “Hutton House has partnered with Microcad through participant placements and product sourcing for many years.  We have always found them to be very accommodating and understanding of participants needs and when we needed equipment very quick to provide solutions that best fit the needs that were requested."
+            </p>
+          </div>
+        </li>
+      
+      </ul>
+    </section>
+
+   </div>
   )
 }
 

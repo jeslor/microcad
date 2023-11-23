7d674162d255236a99c3622399c989489898e0f1
Jeslor <jbestius@gmail.com>
2023-11-23 16:57:12 +0300

    try again with without the logic

--- app/account/[id]/page.tsx
+++ app/account/[id]/page.tsx
@@ -12,97 +12,104 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 
 
 const page = () => {
-  const [sections, setSections] = useState<any>({
-    personalInformation: true,
-    billingInformation: false,
-    orderHistory: false,
-    giftCards: false,
-  });
-    const [user, setUser] = useState<any>(null);
-    const session = useSession();
+  // const [sections, setSections] = useState<any>({
+  //   personalInformation: true,
+  //   billingInformation: false,
+  //   orderHistory: false,
+  //   giftCards: false,
+  // });
+  //   const [user, setUser] = useState<any>(null);
+  //   const session = useSession();
 
-    const {id} = useParams();
-    const getUser = async () => {
-      console.log('reached');
+  //   const {id} = useParams();
+  //   const getUser = async () => {
+  //     console.log('reached');
       
-      const res = await getLoggedInUser(id as string);
-      setUser(res);
-    }
+  //     const res = await getLoggedInUser(id as string);
+  //     setUser(res);
+  //   }
 
-    useEffect(()=>{
-      getUser();
-    }, [id])
+  //   useEffect(()=>{
+  //     getUser();
+  //   }, [id])
     
 
-    const handleSwitchSection = (section: string) => {
-      const newSections = {...sections};
-      Object.keys(newSections).forEach(key => {
-        if(key === section){
-          newSections[key] = true;
-        }else{
-          newSections[key] = false;
-        }
-      })
-      setSections(newSections);
-    }
+  //   const handleSwitchSection = (section: string) => {
+  //     const newSections = {...sections};
+  //     Object.keys(newSections).forEach(key => {
+  //       if(key === section){
+  //         newSections[key] = true;
+  //       }else{
+  //         newSections[key] = false;
+  //       }
+  //     })
+  //     setSections(newSections);
+  //   }
 
 
-    const sectionClasses = (section: string) => {
-      return sections[section] ? styles.activeSection : "";
-    }
+  //   const sectionClasses = (section: string) => {
+  //     return sections[section] ? styles.activeSection : "";
+  //   }
     
     
     
-  return  user === null?(
-      <Spinner />
-    ):(
-
-    <div className={styles.accountContent}>
-      <div className={styles.accountLeft}>
-        <div className={styles.profilePhoto}>
-          <img src="/static/media/icons/user.svg" alt="" />
-        </div>
-        <h2 className={styles.userName}>
-            {`${user?.firstName} ${user?.lastName}`}
-        </h2>
-          <p className={styles.userEmail}> {user?.email}</p>
-
-        <div className={styles.accountDetails}>
-            <ul>
-              <li className={`${sectionClasses('personalInformation')}`}  onClick={()=>handleSwitchSection("personalInformation")}>
-                Persional Information
-              </li>
-              <li className={`${sectionClasses('billingInformation')}`}  onClick={()=>handleSwitchSection("billingInformation")}>
-                Billing & Payments
-              </li>
-              <li className={`${sectionClasses('orderHistory')}`}  onClick={()=>handleSwitchSection("orderHistory")}>
-                Oreder history
-              </li >
-              <li className={`${sectionClasses('giftCards')}`}  onClick={()=>handleSwitchSection("giftCards")}>
-                Gift Cards
-              </li>
-              
-            </ul>
-        </div>
-
-
-      </div>
-      <div className={styles.accountRight}>
-        {
-          sections.personalInformation && <PersonalInformation user={user} />
-        }
-        {
-          sections.billingInformation && <BillingInformation user={user} />
-        }
-        {
-          sections.orderHistory && <OrderHistory user={user} />
-        }
-        {
-          sections.giftCards && <GiftcardInformation user={user} />
-        }
-      </div>
-    </div>
-  )
+  return <h1>Ussseerrr</h1>
 }
 
 export default page
+
+
+
+
+
+
+// user === null?(
+//   <Spinner />
+// ):(
+
+// <div className={styles.accountContent}>
+//   <div className={styles.accountLeft}>
+//     <div className={styles.profilePhoto}>
+//       <img src="/static/media/icons/user.svg" alt="" />
+//     </div>
+//     <h2 className={styles.userName}>
+//         {`${user?.firstName} ${user?.lastName}`}
+//     </h2>
+//       <p className={styles.userEmail}> {user?.email}</p>
+
+//     <div className={styles.accountDetails}>
+//         <ul>
+//           <li className={`${sectionClasses('personalInformation')}`}  onClick={()=>handleSwitchSection("personalInformation")}>
+//             Persional Information
+//           </li>
+//           <li className={`${sectionClasses('billingInformation')}`}  onClick={()=>handleSwitchSection("billingInformation")}>
+//             Billing & Payments
+//           </li>
+//           <li className={`${sectionClasses('orderHistory')}`}  onClick={()=>handleSwitchSection("orderHistory")}>
+//             Oreder history
+//           </li >
+//           <li className={`${sectionClasses('giftCards')}`}  onClick={()=>handleSwitchSection("giftCards")}>
+//             Gift Cards
+//           </li>
+          
+//         </ul>
+//     </div>
+
+
+//   </div>
+//   <div className={styles.accountRight}>
+//     {
+//       sections.personalInformation && <PersonalInformation user={user} />
+//     }
+//     {
+//       sections.billingInformation && <BillingInformation user={user} />
+//     }
+//     {
+//       sections.orderHistory && <OrderHistory user={user} />
+//     }
+//     {
+//       sections.giftCards && <GiftcardInformation user={user} />
+//     }
+//   </div>
+// </div>
+// )

2917493afc311ce1cc171ab3cf3c44b5c65968e9
Jeslor <jbestius@gmail.com>
2023-12-16 14:33:39 +0300

    re-add the old view port

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: `Microcad Computer Corp`,
   description: 'Microcad Computer Corp is a nationwide distributor of computer products. Established since 1991, we have the experience and the means to provide you with the latest products and technologies to cater to all of your home/business applications.',
-  keywords: "refurbished london computer managed.services repair computer.repair london.ontario.computer.repair london.ontario.computers refurbished.computers.london"
+  keywords: "refurbished london computer managed.services repair computer.repair london.ontario.computer.repair london.ontario.computers refurbished.computers.london",
+  viewport: 'width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no',
 }
 
 export default async function RootLayout({
